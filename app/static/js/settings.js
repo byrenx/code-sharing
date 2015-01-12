5889dee724d17e797fd74af2b4324e21ca8b65f3
Erika Cura <erika.cura@cloudsherpas.com>
2015-01-12 17:03:37 +0800

    Enhance loader

--- app/static/js/settings.js
+++ app/static/js/settings.js
@@ -145,39 +145,3 @@ $(function () {
     $('#pop_hover').click();
 
 });
-
-/* For loading animation */
-var cSpeed = 9;
-var cWidth = 256;
-var cHeight = 85;
-var cTotalFrames = 12;
-var cFrameWidth = 256;
-var cImageSrc = '/static/img/sprites.gif';
-var cImageTimeout = false;
-
-function startAnimation() {
-
-    document.getElementById('preload').innerHTML = '<canvas id="canvas" width="'+cWidth+'" height="'+cHeight+'"><p>Your browser does not support the canvas element.</p></canvas>';
-
-    //FPS = Math.round(100/(maxSpeed+2-speed));
-    FPS = Math.round(100 / cSpeed);
-    SECONDS_BETWEEN_FRAMES = 1 / FPS;
-    g_GameObjectManager = null;
-    g_run = genImage;
-    g_run.width = cTotalFrames * cFrameWidth;
-    genImage.onload = function () { cImageTimeout = setTimeout(fun, 0) };
-		initCanvas();
-}
-
-
-function imageLoader(s, fun)// Pre-loads the sprites image
-{
-    clearTimeout(cImageTimeout);
-    cImageTimeout = 0;
-    genImage = new Image();
-    genImage.onload = function (){cImageTimeout=setTimeout(fun, 0)};
-    genImage.onerror = new Function('alert(\'Could not load the image\')');
-	genImage.src = s;
-}
-
-new imageLoader(cImageSrc, 'startAnimation()');
