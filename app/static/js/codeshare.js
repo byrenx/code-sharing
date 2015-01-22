bc6feaa76b752bf3cccfb51f994324a9df965c37
Erika Cura <erika.cura@cloudsherpas.com>
2015-01-22 15:11:37 +0800

    Hide loader when Firepad is ready

--- app/static/js/codeshare.js
+++ app/static/js/codeshare.js
@@ -58,7 +58,7 @@ function init(theme) {
 
     firepad.on('ready', function () {
         // Firepad is ready
-        $("#preload").hide();
+        waitingDialog.hide();
         firepadRef.update({ startedAt: Firebase.ServerValue.TIMESTAMP });
         firepadRef.onDisconnect().update({ updatedAt: Firebase.ServerValue.TIMESTAMP });
     });
@@ -125,6 +125,58 @@ function firebasemode() {
 	    }
     });
 }
+
+// Loader
+var waitingDialog = (function ($) {
+
+    // Creating modal dialog's DOM
+    var $dialog = $(
+        '<div class="modal fade" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-hidden="true" style="padding-top:15%; overflow-y:visible;">' +
+        '<div class="modal-dialog modal-m">' +
+        '<div class="modal-content">' +
+            '<div class="modal-header"><h4 style="margin:0;"></h4></div>' +
+            '<div class="modal-body">' +
+                '<div class="progress progress-striped active" style="margin-bottom:0;"><div class="progress-bar" style="width: 100%"></div></div>' +
+            '</div>' +
+        '</div></div></div>');
+
+    return {
+        /**
+         * Opens our dialog
+         */
+        show: function (message, options) {
+            // Assigning defaults
+            var settings = $.extend({
+                dialogSize: 'm',
+                progressType: ''
+            }, options);
+            if (typeof message === 'undefined') {
+                message = 'Please wait...';
+            }
+            if (typeof options === 'undefined') {
+                options = {};
+            }
+            // Configuring dialog
+            $dialog.find('.modal-dialog').attr('class', 'modal-dialog').addClass('modal-' + settings.dialogSize);
+            $dialog.find('.progress-bar').attr('class', 'progress-bar');
+            if (settings.progressType) {
+                $dialog.find('.progress-bar').addClass('progress-bar-' + settings.progressType);
+            }
+            $dialog.find('h4').text(message);
+            // Opening dialog
+            $dialog.modal();
+        },
+        /**
+         * Closes dialog
+         */
+        hide: function () {
+            $dialog.modal('hide');
+        }
+    }
+
+})(jQuery);
+// Show loader
+waitingDialog.show();
 
 // Fire initialization of Firepad
 init('default');
